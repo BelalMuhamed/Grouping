9adcff8aea0fec624731c67b9fb469450d4b7995
BelalBasal <basdibrahim@gmail.com>
2024-11-23 14:19:58 +0200

    v01

--- Grouping/Grouping.cpp
+++ Grouping/Grouping.cpp
@@ -1,1 +1,54 @@
 
+#include <iostream>
+using namespace std;
+void Groups(string Str)
+{
+	int counter = 0;
+	if (!Str.empty())
+	{
+
+		for (int i = 0; i < Str.length();i++)
+		{
+			if (counter == Str.length())
+				break;
+			for (int j = 0;j < Str.length();j++)
+			{
+				counter++;
+				if (Str[i] == Str[j])
+				{
+					cout << Str[i];
+
+				}
+				else
+				{
+					cout << "\n";
+
+					i = j;
+					cout << Str[i];
+				}
+
+
+
+			}
+
+		}
+	}
+	else
+	{
+		cout << "the string is empty";
+	}
+}
+
+
+int main()
+{
+	Groups("absdef");
+	cout << "\n ********************* \n";
+	Groups("111223344aabb");
+	cout << "\n ********************* \n";
+	Groups("HHHHHHHH");
+
+
+
+}
+
